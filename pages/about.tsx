3fbec8f9b01feccdef4a67ea2e007c3b87570a71
BJCDevelop <bjcdevelop@gmail.com>
2022-03-22 16:10:51 -0400

    Typescript migration by Braulio Chavez

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+function About() {
+    return(
+        <Layout>
+            <div>About</div>
+        </Layout>
+    )
+  }
+  
+export default About
+  
